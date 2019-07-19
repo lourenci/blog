8a50ba69718b4d4d8da9d1cbdaf62c9ae61d560e
Leandro Lourenci <leandro@lourenci.com>
2019-07-19 12:04:05 -0300

    Change blog icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -116,7 +116,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/command-icon.png`,
       },
     },
     `gatsby-plugin-offline`,
